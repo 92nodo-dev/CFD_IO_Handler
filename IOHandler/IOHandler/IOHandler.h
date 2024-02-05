7fac18bf9e2c20c0b0a26671a425ccf553f0d853
92nodo-dev <92nodo30@gmail.com>
2024-02-05 12:09:14 +0900

    initialize

---
+++ IOHandler/IOHandler/IOHandler.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+
+class IOHandler 
+{
+private :
+	std::string filename;
+	std::string* resultType;
+	double* result;
+
+public :
+	IOHandler(std::string filename) { this->filename = filename; };
+	~IOHandler() { free(resultType); free(result); };
+	void get_from_vtk();
+	void get_from_vtu();
+};
---
+++ IOHandler/IOHandler/ResultType.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+enum class ResultType :int
+{
+	DENSITY = 0,
+	POSITION,
+	VELOCITY
+};
