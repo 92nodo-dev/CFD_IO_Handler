cfc048eeb8a3cd971105876b4280484b1285c968
92nodo-dev <92nodo30@gmail.com>
2024-02-08 10:07:43 +0900

    Update IOHandler.cpp

--- IOHandler/IOHandler/IOHandler.cpp
+++ IOHandler/IOHandler/IOHandler.cpp
@@ -2,11 +2,12 @@
 
 void IOHandler::get_from_vtu() {
 	std::ifstream file(filename);
-
+	bool isInsidePoint = false;
 	if (file.is_open()) {
 		while (std::getline(file, line)) {
 			if (line.find("<DataArray") != std::string::npos) {
-
+				// npos는 못찾은 경우
+				// 이런식으로 찾아들어가면될듯 line 별로
 			}
 		}
 	}
